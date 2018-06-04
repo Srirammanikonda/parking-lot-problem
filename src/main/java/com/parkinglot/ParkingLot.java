a5ad1c407036471a7b007b4d83bdb6459808f4c3
Smanikonda <Smanikonda@manh.com>
2018-06-05 00:55:04 +0530

    Initial Check-in

---
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -0,0 +1,18 @@
+package com.parkinglot;
+
+import java.util.*;
+
+/**
+ * Created by Sriram on 05/06/18.
+ */
+public class ParkingLot {
+    int MAX_SIZE = 0;
+    private class Car {
+        String regNo;
+        String color;
+        public Car(String regNo, String color) {
+            this.regNo = regNo;
+            this.color = color;
+        }
+    }
+}
